3b822f88ba876050b1f826469e104582ca020e5b
holequotin <69898497+holequotin@users.noreply.github.com>
2021-08-22 10:00:17 +0700

    Create Bubble_sort.cpp

---
+++ Sort/Bubble_sort.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+//hàm nhập mảng
+void nhap(int n,int arr[]){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+//hàm xuất mảng
+void xuat(int n,int arr[]){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+//hàm đổi chỗ
+void swap(int *a,int *b){
+    int c=*a;
+    *a=*b;
+    *b=c;
+}
+void Select_Sort(int n,int arr[]){
+    for(int i=0;i<n-1;i++){
+        int min_index=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[min_index]) min_index=j;
+        }
+        swap(&arr[i],&arr[min_index]);
+    }
+}
+void Bubble_Sort(int n,int arr[]){
+    for(int i=n-1;i>0;i--){
+        for(int j=0;j<i;j++){
+            if(arr[j]>arr[j+1]) swap(&arr[j],&arr[j+1]);
+        }
+    }
+}
+int main() {
+int n,arr[100];
+cin>>n;
+nhap(n,arr);
+// Select_Sort(n,arr);
+Bubble_Sort(n,arr);
+xuat(n,arr);
+    return 0;
+}
