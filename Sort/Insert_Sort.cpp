21d9f6285b9c35b3c075b5a7cd0e41ea74111d72
holequotin <69898497+holequotin@users.noreply.github.com>
2021-08-22 09:47:12 +0700

    Update Insert_Sort.cpp

--- Sort/Insert_Sort.cpp
+++ Sort/Insert_Sort.cpp
@@ -1,1 +1,40 @@
+#include<iostream>
+
+using namespace std;
+//hàm nhập mảng
+void nhap(int n,int arr[]){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+//hàm in ra dãy
+void xuat(int n,int arr[]){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+//hàm đổi chỗ
+void swap(int *a,int *b){
+    int c=*a;
+    *a=*b;
+    *b=c;
+}
+void InsertSort(int n,int arr[]){
+    for(int i=1;i<n;i++){
+        int index=i;
+        while (arr[index]<arr[index-1]&&index>0)
+        {
+            swap(&arr[index],&arr[index-1]);
+            index--;
+        }
+    }
+}
+int main() {
+int n,arr[100];
+cin>>n;
+nhap(n,arr);
+InsertSort(n,arr);
+xuat(n,arr);
+    return 0;
+}
 
